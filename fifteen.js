4654d33061737781f3b5891a5ae0be12a5ceb419
unknown <srosengren@STRSEBROS.stratiteq.corp>
2012-07-09 17:18:40 +0200

    Style refactoring and winannouncement
    Renamed the square class to tile.
    Implemented the "correct" class for #gameField.
    Implemented click "off" when the sequenceLength >= 15

--- fifteen.js
+++ fifteen.js
@@ -1,53 +1,56 @@
 
 var fifteenGame = {
-	emptySquare: null,
+	emptytile: null,
 	values: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
 	emptyIndex: 0,
 	correctSequenceLength: 0,
 	init: function(){
 		for(var i = this.values.length; i > 0;i--){
 			var value = this.values.splice(Math.floor(Math.random() * i),1);
-			$('#gameField').append('<div class="square" data-index="'+(16 -i)+'" data-value="'+value+'"><h3>'+value+'</h3></div>');
+			$('#gameField').append('<div class="tile" data-index="'+(16 -i)+'" data-value="'+value+'"><h3>'+value+'</h3></div>');
 		}
-		this.emptySquare = $('#gameField .square.empty');
+		this.emptytile = $('#gameField .tile.empty');
 		this.toggleClassesAroundEmpty();
 	},
 	toggleClassesAroundEmpty: function(){
-			$('#gameField .square').removeClass('over under left right');
-			$('#gameField .square[data-index="'+(parseInt(this.emptyIndex) +1)+'"]').addClass('right');
-			$('#gameField .square[data-index="'+(parseInt(this.emptyIndex) +4)+'"]').addClass('under');
-			$('#gameField .square[data-index="'+(parseInt(this.emptyIndex) -1)+'"]').addClass('left');
-			$('#gameField .square[data-index="'+(parseInt(this.emptyIndex) -4)+'"]').addClass('over');
+			$('#gameField .tile').removeClass('over under left right');
+			$('#gameField .tile[data-index="'+(parseInt(this.emptyIndex) +1)+'"]').addClass('right');
+			$('#gameField .tile[data-index="'+(parseInt(this.emptyIndex) +4)+'"]').addClass('under');
+			$('#gameField .tile[data-index="'+(parseInt(this.emptyIndex) -1)+'"]').addClass('left');
+			$('#gameField .tile[data-index="'+(parseInt(this.emptyIndex) -4)+'"]').addClass('over');
 	},
 	winCheck: function(){
-		var squareElements = $('#gameField .square'); 
+		var tileElements = $('#gameField .tile'); 
 		var value = 0;
 		var i = 0;
-		$(squareElements).removeClass('correct');
-		for(; i < squareElements.length && value +1 == parseInt($(squareElements[i]).attr('data-value'));i++){
+		$(tileElements).removeClass('correct');
+		for(; i < tileElements.length && value +1 == parseInt($(tileElements[i]).attr('data-value'));i++){
 			value++;
-			$(squareElements[i]).addClass('correct');
+			$(tileElements[i]).addClass('correct');
 		}
 		return i;
 	},
-	squareClick: function(){
-		var squareIndex = parseInt($(this).attr('data-index'));
-		if(fifteenGame.emptyIndex +1 == squareIndex || fifteenGame.emptyIndex -1 == squareIndex ||
-		 fifteenGame.emptyIndex + 4 == squareIndex || fifteenGame.emptyIndex -4 == squareIndex){
-			fifteenGame.emptySquare.html($(this).html());
+	tileClick: function(){
+		var tileIndex = parseInt($(this).attr('data-index'));
+		if(fifteenGame.emptyIndex +1 == tileIndex || fifteenGame.emptyIndex -1 == tileIndex ||
+		 fifteenGame.emptyIndex + 4 == tileIndex || fifteenGame.emptyIndex -4 == tileIndex){
+			fifteenGame.emptytile.html($(this).html());
 			$(this).html('');
 
-			fifteenGame.emptySquare.attr('data-value',$(this).attr('data-value'));
+			fifteenGame.emptytile.attr('data-value',$(this).attr('data-value'));
 			$(this).attr('data-value',16);
 
-			fifteenGame.emptySquare.removeClass('empty');
+			fifteenGame.emptytile.removeClass('empty');
 			$(this).addClass('empty');
-			fifteenGame.emptySquare = $(this);
+			fifteenGame.emptytile = $(this);
 
-			fifteenGame.emptyIndex = parseInt(fifteenGame.emptySquare.attr('data-index'));
+			fifteenGame.emptyIndex = parseInt(fifteenGame.emptytile.attr('data-index'));
 
 			fifteenGame.toggleClassesAroundEmpty();
 		 	fifteenGame.sequenceLength = fifteenGame.winCheck();
+		 	if(sequenceLength >= 15){
+		 		$('#gameField').off('click','.tile',fifteenGame.tileClick);
+		 	}
 			$('#gameStatus .sequenceLength-js').text(fifteenGame.sequenceLength);
 			$('#gameStatus .sequenceRemaining-js').text(16 - fifteenGame.sequenceLength);
 		}
